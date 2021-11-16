6c083dac5f8fcfa254f3a909529eb748c53afe96
claaspetersen <claas.petersen@typefox.io>
2021-11-16 14:59:23 +0000

    fixed imports on js generator

--- src/cli/generator-js.ts
+++ src/cli/generator-js.ts
@@ -1,19 +1,18 @@
 import fs from 'fs';
-import { AstNode, CompositeGeneratorNode, NL, processGeneratorNode, reflection } from 'langium';
-import { JSElements, JSFunction, Popup, SimpleUi, SimpleUiAstType } from '../language-server/generated/ast';
+import { AstNode, CompositeGeneratorNode, NL, processGeneratorNode } from 'langium';
+import { JSFunction, Popup, SimpleUi, SimpleUiAstType, reflection } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 
 export type GenerateFunctions = {
     [key in SimpleUiAstType]?:(el: AstNode)=>string|CompositeGeneratorNode
 }
 
-export function generateJS(model: SimpleUi, filePath: string, destination: string | undefined): string{
+export function generateJS(model: SimpleUi, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
     const generatedFilePath = `${data.destination}script.js`;
 
     const fileNode = new CompositeGeneratorNode();
-    fileNode.append('// Test');
-    generateJSFunc(model, fileNode);
+    generateJSFunc(model, fileNode)
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
@@ -32,7 +31,7 @@ const functionFunc = (functionEL: AstNode) => {
     const fileNode = new CompositeGeneratorNode()
     fileNode.append(`function ${el.functionname}() {`, NL);
     fileNode.indent(functioncontent => {
-        generateFunc(el.content, functioncontent);
+        generateJSFunc(el.content, functioncontent);
     });
     fileNode.append('}', NL);
     return fileNode
@@ -43,26 +42,10 @@ export const generateJSFunctions: GenerateFunctions = {
     JSFunction: functionFunc
 }
 
+
 export function generateJSFunc(model: SimpleUi, bodyNode: CompositeGeneratorNode) {
     const suiTypes = reflection.getAllTypes();
     model.jselements.forEach(el => {
-        suiTypes.forEach(suiType => {
-            const t = suiType as SimpleUiAstType;
-            const isInstance = reflection.isInstance(el, t);
-            if (isInstance) {
-                const func = generateJSFunctions[t];
-                if (func) {
-                    const content = func(el);
-                    bodyNode.append(content, NL);
-                }
-            }
-        })
-    })
-}
-
-export function generateFunc(model: JSElements, bodyNode: CompositeGeneratorNode) {
-    const suiTypes = reflection.getAllTypes();
-    model.jsfunction.forEach(el => {
         suiTypes.forEach(suiType => {
             const t = suiType as SimpleUiAstType;
             const isInstance = reflection.isInstance(el, t);
