f4bc5921901f9ca363b9a2d61a6743c36d36fc84
claaspetersen <claas.petersen@typefox.io>
2022-01-03 15:39:21 +0000

    added typechecking to js generator

--- src/language-server/simple-ui-validator.ts
+++ src/language-server/simple-ui-validator.ts
@@ -1,5 +1,5 @@
 import { ValidationAcceptor, ValidationCheck, ValidationRegistry } from 'langium';
-import { SimpleUi, SimpleUIAstType, UseComponent, Component, isStringExpression, isNumberExpression } from './generated/ast';
+import { SimpleUi, SimpleUIAstType, UseComponent, Component, isStringExpression, isNumberExpression, Button } from './generated/ast';
 import { SimpleUiServices } from './simple-ui-module';
 
 /**
@@ -15,7 +15,8 @@ export class SimpleUiValidationRegistry extends ValidationRegistry {
         super(services);
         const validator = services.validation.SimpleUiValidator;
         const checks: SimpleUiChecks = {
-            UseComponent: validator.checkUseComponent
+            UseComponent: validator.checkUseComponent,
+            Button: validator.checkButton
         };
         this.register(checks, validator);
     }
@@ -28,7 +29,7 @@ export class SimpleUiValidator {
     checkUseComponent(el: UseComponent, accept: ValidationAcceptor): void {
         const refContent = el.component.ref?.content as SimpleUi
         const refParameters = (refContent.$container as Component).parameters;
-
+        
         // Check type of parameters
         el.arguments.forEach(function (el, index) {
             if (isNumberExpression(el)) {
@@ -47,6 +48,27 @@ export class SimpleUiValidator {
                 else {
                     accept('error', `Wrong parameter type 'string', expected parameter of type '${refParameters[index].type}'`, { node: el, property: 'value'});
                     return
+                }
+            }
+        })
+    }
+    checkButton(el: Button, accept: ValidationAcceptor): void {
+        const refParameters = el.onclickaction?.ref?.parameters
+        el.arguments.forEach(function (el, index) {
+            if (isNumberExpression(el)) {
+                if (refParameters![index].type === 'number') {
+                    return
+                } else {
+                    accept('error', `Wrong parameter type 'number', expected parameter of type '${refParameters![index].type}'`, {node: el, property: 'value'})
+                    return
+                }
+            }
+            else if (isStringExpression(el)) {
+                if (refParameters![index].type === 'string') {
+                    return
+                }
+                else {
+                    accept('error', `Wrong parameter type 'string', expected parameter of type '${refParameters![index].type}'`, { node: el, property: 'value'});
                 }
             }
         })
