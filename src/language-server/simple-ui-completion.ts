5a12b94e35e4e67735ee9f61569b5ec00d968e79
emilkrebs <onlime@gmx.de>
2023-05-07 21:40:06 +0200

    fixed the autocompletion

--- src/language-server/simple-ui-completion.ts
+++ src/language-server/simple-ui-completion.ts
@@ -1,111 +1,129 @@
-import { AstNode, CompletionAcceptor, DefaultCompletionProvider, NameProvider, LangiumServices, AstNodeDescription, findLeafNodeAtOffset, findNextFeatures, LangiumDocument, MaybePromise, stream, flattenCst } from "langium";
+import { CompletionAcceptor, DefaultCompletionProvider, NameProvider, LangiumServices, CompletionContext, MaybePromise, NextFeature, LangiumDocument, findLeafNodeAtOffset, getEntryRule, stream } from "langium";
 import fs from "fs"
 import path from "path"
-import { CompletionItem, CompletionItemKind, CompletionList } from "vscode-languageserver-types";
-import { CompletionParams } from "vscode-languageclient";
-import { CSSClasses, isCSSClasses } from "./generated/ast";
-import { AbstractElement, isCrossReference, isKeyword, isParserRule, isRuleCall } from "langium/lib/grammar/generated/ast";
+import { isCSSClasses } from "./generated/ast";
+import { AbstractElement, isKeyword, isRuleCall, isCrossReference, CrossReference } from "langium/lib/grammar/generated/ast";
+import { CompletionItem, CompletionItemKind, CompletionList, CompletionParams } from 'vscode-languageserver';
 
-export class SimpleUICompletionProvider extends DefaultCompletionProvider{
+export class SimpleUICompletionProvider extends DefaultCompletionProvider {
     protected readonly nameProvider: NameProvider;
-    constructor(services:LangiumServices){
+    constructor(services: LangiumServices) {
         super(services);
         this.nameProvider = services.references.NameProvider;
     }
 
-    getCompletion(document: LangiumDocument, params: CompletionParams): Promise<CompletionList> {
+    async getCompletion(document: LangiumDocument, params: CompletionParams): Promise<CompletionList | undefined> {
         const root = document.parseResult.value;
         const cst = root.$cstNode;
+        if (!cst) {
+            return undefined;
+        }
         const items: CompletionItem[] = [];
-        const offset = document.textDocument.offsetAt(params.position);
-        const acceptor = (value: string | AstNode | AstNodeDescription, item?: Partial<CompletionItem>) => {
-            const completionItem = this.fillCompletionItem(document.textDocument, offset, value, item);
+        const textDocument = document.textDocument;
+        const text = textDocument.getText();
+        const offset = textDocument.offsetAt(params.position);
+        const acceptor: CompletionAcceptor = value => {
+            const completionItem = this.fillCompletionItem(textDocument, offset, value);
             if (completionItem) {
                 items.push(completionItem);
             }
         };
-        if (cst) {
-            const node = findLeafNodeAtOffset(cst, offset);
-            if (node) {
-                const features = findNextFeatures(this.buildFeatureStack(node));
-                const commonSuperRule = this.findCommonSuperRule(node);
-                // In some cases, it is possible that we do not have a super rule
-                if (commonSuperRule) {
-                    // TODO flattenCst
-                    const flattened = flattenCst(commonSuperRule.node).filter(e => e.offset < offset);
-                    const possibleFeatures = this.ruleInterpreter.interpretRule(commonSuperRule.rule, [...flattened], offset);
-                    // Remove features which we already identified during parsing
-                    const partialMatches = possibleFeatures.filter(e => {
-                        const match = this.ruleInterpreter.featureMatches(e, flattened[flattened.length - 1], offset);
-                        return match === 'partial' || match === 'both';
-                    });
-                    const notMatchingFeatures = possibleFeatures.filter(e => !partialMatches.includes(e));
-                    features.push(...partialMatches);
-                    features.push(...notMatchingFeatures.flatMap(e => findNextFeatures([e])));
-                }
-                if (node.end > offset) {
-                    features.push(node.feature);
-                }
-                stream(features).distinct(e => {
-                    if (isKeyword(e)) {
-                        return e.value;
-                    } else {
-                        return e;
-                    }
-                }).forEach(e => this.completionFor(findRelevantNode(node), e, acceptor));
-            } else {
-                // The entry rule is the first parser rule
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                const parserRule = this.grammar.rules.find(e => isParserRule(e))!;
-                this.completionForRule(undefined, parserRule, acceptor);
-            }
+
+        const node = findLeafNodeAtOffset(cst, this.backtrackToAnyToken(text, offset));
+
+        const context: CompletionContext = {
+            document,
+            textDocument,
+            node: node?.element,
+            offset,
+            position: params.position
+        };
+
+        if (!node) {
+            const parserRule = getEntryRule(this.grammar)!;
+            await this.completionForRule(context, parserRule, acceptor);
+            return CompletionList.create(items, true);
         }
-        const filteredItems : CompletionItem[]= [];
-        
+
+        const parserStart = this.backtrackToTokenStart(text, offset);
+        const beforeFeatures = this.findFeaturesAt(textDocument, parserStart);
+        let afterFeatures: NextFeature[] = [];
+        const reparse = this.canReparse() && offset !== parserStart;
+        if (reparse) {
+            afterFeatures = this.findFeaturesAt(textDocument, offset);
+        }
+
+        const distinctionFunction = (element: NextFeature) => {
+            if (isKeyword(element.feature)) {
+                return element.feature.value;
+            } else {
+                return element.feature;
+            }
+        };
+
+        await Promise.all(
+            stream(beforeFeatures)
+                .distinct(distinctionFunction)
+                .map(e => this.completionFor(context, e, acceptor))
+        );
+
+        if (reparse) {
+            await Promise.all(
+                stream(afterFeatures)
+                    .exclude(beforeFeatures, distinctionFunction)
+                    .distinct(distinctionFunction)
+                    .map(e => this.completionFor(context, e, acceptor))
+            );
+        }
+
+        const filteredItems: CompletionItem[] = [];
+
         items.forEach(item => {
-            if(!filteredItems.some(i => i.label === item.label)){
+            if (!filteredItems.some(i => i.label === item.label)) {
                 filteredItems.push(item);
             }
         })
-        
-        return CompletionList.create(filteredItems, true);
+
+        return CompletionList.create(items, true);
     }
 
 
-    protected completionFor(astNode: AstNode | undefined, feature: AbstractElement, acceptor: CompletionAcceptor): void {
-        if (isKeyword(feature)) {          
-            this.completionForKeyword(feature, astNode, acceptor);                    
-        } 
+    protected completionFor(context: CompletionContext, next: NextFeature<AbstractElement>, acceptor: CompletionAcceptor): MaybePromise<void> {
+        const feature = next.feature;
+        if (isKeyword(feature)) {
+            return this.completionForKeyword(context, feature, acceptor);
+        }
         else if (isRuleCall(feature) && feature.rule.ref) {
-            if(isCSSClasses(astNode)){
-                this.completionForCSSClasses(astNode, acceptor);
+            if (isCSSClasses(context.node)) {
+                this.completionForCSSClasses(context, acceptor);
             }
             else {
-                return this.completionForRule(astNode, feature.rule.ref, acceptor);
+                return this.completionForRule(context, feature.rule.ref, acceptor);
             }
         }
-        else if (isCrossReference(feature) && astNode) {
-            this.completionForCrossReference(feature, astNode, acceptor);
+        else if (isCrossReference(feature) && context.node) {
+            return this.completionForCrossReference(context, next as NextFeature<CrossReference>, acceptor);
         }
     }
-    completionForCSSClasses(astNode: CSSClasses, acceptor: CompletionAcceptor){
+
+    completionForCSSClasses(context: CompletionContext, acceptor: CompletionAcceptor) {
         let cssClasses = this.getCSSClasses();
-        
-        cssClasses.forEach(element => {
-                acceptor({kind: CompletionItemKind.Value, detail: 'CSS Class'});
-        
+        cssClasses.forEach((element) => {
+            acceptor({
+                label: element,
+                kind: CompletionItemKind.Value, 
+                detail: 'CSS Class',
+                sortText: '1'
+            });
         });
     }
 
     getCSSClasses(): string[] {
-        
-        const fileContent = fs.readFileSync(path.resolve(__dirname + '../../../src/assets/base.css'),'utf8');
+        const fileContent = fs.readFileSync(path.resolve(__dirname + '../../../src/assets/base.css'), 'utf8');
         let regex = /(?<=\.)([a-zA-Z0-9]*([-]*[a-zA-Z0-9]*)*)/gm;
         let cssClasses = fileContent.match(regex);
-        
-        console.error(cssClasses);
-        if(cssClasses == null) return [];
-        
+        if (cssClasses == null) return [];
+
         return cssClasses;
     }
 }
